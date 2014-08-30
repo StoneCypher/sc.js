7ff1e043459e0fa9b1d8fb1bc4c567640d0bc7e7
John Haugeland <stonecypher@gmail.com>
2014-08-30 16:41:19 -0700

    .gitignore, bump script

---
+++ bump.sh
@@ -0,0 +1,11 @@
+
+cd /c/projects/sc.js
+echo $1
+echo   git adding
+git add . -A
+echo   npm version patching
+npm version patch -m "$1"
+echo   git re-adding
+git add . -A
+echo   git committing
+git commit -m "$1"
