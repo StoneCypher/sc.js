bdc1ff9f0a5c86a84f20c74e23717764c152cf3a
John Haugeland <stonecypher@gmail.com>
2014-08-30 18:00:45 -0700

    Clean tests

--- test/sc-tests.js
+++ test/sc-tests.js
@@ -6,8 +6,15 @@ var vows   = require('vows'),
     assert = require('assert');
 
 vows.describe('es5toString').addBatch({
-    '1'   : { topic: 1,   'yields "1"'   : function (topic) { assert.equal(topic, "1");   } },
-    '0.5' : { topic: 0.5, 'yields "0.5"' : function (topic) { assert.equal(topic, "0.5"); } },
+    '1'       : { topic: sc.es5protoToString(1),       'yields "[object Number]"' : function (topic) { assert.equal(topic, "[object Number]"); } },
+    '0.5'     : { topic: sc.es5protoToString(0.5),     'yields "[object Number]"' : function (topic) { assert.equal(topic, "[object Number]"); } },
+    '[1,2,3]' : { topic: sc.es5protoToString([1,2,3]), 'yields "[Object Array]"'  : function (topic) { assert.equal(topic, "[object Array]");  } },
+}).export(module);
+
+vows.describe('getClass').addBatch({
+    '1'       : { topic: sc.getClass(1),       'yields "Number"' : function (topic) { assert.equal(topic, "Number"); } },
+    '0.5'     : { topic: sc.getClass(0.5),     'yields "Number"' : function (topic) { assert.equal(topic, "Number"); } },
+    '[1,2,3]' : { topic: sc.getClass([1,2,3]), 'yields "Array"'  : function (topic) { assert.equal(topic, "Array");  } },
 }).export(module);
 
 // expandedToString([1,2,3]);
