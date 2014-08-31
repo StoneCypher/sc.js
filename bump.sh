b368213caa989322fa3b77356d1ff122f9b8eca9
John Haugeland <stonecypher@gmail.com>
2014-08-30 18:01:29 -0700

    Improved bump script

--- bump.sh
+++ bump.sh
@@ -3,9 +3,10 @@ cd /c/projects/sc.js
 echo $1
 echo "  git adding"
 git add . -A
+git commit -m "$1"
 echo "  npm version patching"
 npm version patch -m "$1"
 echo "  git re-adding"
 git add . -A
 echo "  git committing"
-git commit -m "$1"
+git commit --no-edit --amend
